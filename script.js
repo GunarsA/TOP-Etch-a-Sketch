9d2f0445bc6c4aeacd5395ff35d57828d4ae0748
Gunārs Ābeltiņš <gunars2003@gmail.com>
2022-04-16 20:41:08 +0300

    Add gradual coloring and tile count choice

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function getTile(tileSize) {
   tile.classList.add('grid-tile');
   tile.style.width = tileSize + 'px';
   tile.style.height = tileSize + 'px';
+  tile.style.backgroundColor = 'black';
+  tile.style.opacity = 0;
   return tile;
 }
 
-function makeGrid(gridWidth = 600, gridHeight = 600, tileSize = 20) {
+function makeGrid(gridWidth = 600, gridHeight = 600, tileSize = 10) {
   const gridContainer = document.querySelector('#grid');
   gridContainer.style.width = gridWidth + "px";
   gridContainer.style.height = gridHeight + "px";
@@ -18,10 +20,22 @@ function makeGrid(gridWidth = 600, gridHeight = 600, tileSize = 20) {
       2)); i++) {
     const temp2 = temp.cloneNode();
     temp2.addEventListener('mouseover', function() {
-      this.style.backgroundColor = 'red';
+      this.style.opacity = parseFloat(this.style.opacity) + 0.2;
     });
     gridContainer.appendChild(temp2);
   }
 }
 
+function clearGrid() {
+  const gridContainer = document.querySelector('#grid');
+  while(gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+  const tilesPerSide = prompt("How many tiles per side?");
+  makeGrid(600, 600, (600 / tilesPerSide) - 2);
+}
+
 makeGrid();
+
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clearGrid);
